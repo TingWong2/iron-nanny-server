13c7afdea0f98eea4e03c903c139d8efda98390e
Amine <aminehouidi90@gmail.com>
2022-02-09 18:07:53 +0100

    heroku app server setup modifications

--- app.js
+++ app.js
@@ -33,4 +33,11 @@ app.use("/api/*", (req, res, next) => {
   next(error);
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+}
+
 module.exports = app;
